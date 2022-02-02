6a09fba4befe532a245de04b03aeb3d2af103a1b
Jody Septiawan <jody@Jodys-MacBook-Air.local>
2022-02-02 14:59:53 +0700

    update

--- server/src/socket/index.js
+++ server/src/socket/index.js
@@ -1,88 +1,97 @@
 // import models
-const {chat, user, profile} = require("../../models")
+const { chat, user, profile } = require('../../models');
 
 const socketIo = (io) => {
-
   // code here
 
+  io.use((socket, next) => {
+    if (socket.handshake.auth && socket.handshake.auth.token) {
+      next();
+    } else {
+      next(new Error('Error: Access denied'));
+    }
+  });
+
   io.on('connection', (socket) => {
-    console.log('client connect: ', socket.id)
+    console.log('client connect: ', socket.id);
 
     // define listener on event load admin contact
-    socket.on("load admin contact", async () => {
+    socket.on('load admin contact', async () => {
       try {
         const adminContact = await user.findOne({
           include: [
             {
               model: profile,
-              as: "profile",
+              as: 'profile',
               attributes: {
-                exclude: ["createdAt", "updatedAt"],
+                exclude: ['createdAt', 'updatedAt'],
               },
             },
           ],
           where: {
-            status: "admin"
+            status: 'admin',
           },
           attributes: {
-            exclude: ["createdAt", "updatedAt", "password"],
+            exclude: ['createdAt', 'updatedAt', 'password'],
           },
         });
 
-        socket.emit("admin contact", adminContact)
+        socket.emit('admin contact', adminContact);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
     });
 
     // define listener on event load customer contact
-    socket.on("load customer contacts", async () => {
+    socket.on('load customer contacts', async () => {
       try {
         let customerContacts = await user.findAll({
           include: [
             {
               model: profile,
-              as: "profile",
+              as: 'profile',
               attributes: {
-                exclude: ["createdAt", "updatedAt"],
+                exclude: ['createdAt', 'updatedAt'],
               },
             },
             {
               model: chat,
-              as: "recipientMessage",
+              as: 'recipientMessage',
               attributes: {
-                exclude: ["createdAt", "updatedAt", "idRecipient", "idSender"],
+                exclude: ['createdAt', 'updatedAt', 'idRecipient', 'idSender'],
               },
             },
             {
               model: chat,
-              as: "senderMessage",
+              as: 'senderMessage',
               attributes: {
-                exclude: ["createdAt", "updatedAt", "idRecipient", "idSender"],
+                exclude: ['createdAt', 'updatedAt', 'idRecipient', 'idSender'],
               },
             },
           ],
           attributes: {
-            exclude: ["createdAt", "updatedAt", "password"],
+            exclude: ['createdAt', 'updatedAt', 'password'],
           },
         });
 
-        customerContacts = JSON.parse(JSON.stringify(customerContacts))
-        customerContacts = customerContacts.map(item => ({
+        customerContacts = JSON.parse(JSON.stringify(customerContacts));
+        customerContacts = customerContacts.map((item) => ({
           ...item,
-          image: item.profile?.image ? process.env.PATH_FILE + item.profile?.image : null
-        }))
-        
-        socket.emit("customer contacts", customerContacts)
+          image: item.profile?.image
+            ? process.env.PATH_FILE + item.profile?.image
+            : null,
+        }));
+
+        socket.emit('customer contacts', customerContacts);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
-    })
+    });
 
-    socket.on("disconnect", () => {
-      console.log("client disconnect")
-    })
-  })
-}
+    socket.on('disconnect', () => {
+      console.log('client disconnect');
+    });
+  });
+};
 
-module.exports = socketIo
+module.exports = socketIo;
